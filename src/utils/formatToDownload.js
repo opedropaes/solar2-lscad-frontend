2f5212c8d0cc83ae5f9111e447f6d9e598c1e418
Pedro <pedromvpaes@gmail.com>
2019-09-10 15:00:45 -0400

    Update packages

--- src/utils/formatToDownload.js
+++ src/utils/formatToDownload.js
@@ -1,71 +1,71 @@
 /* eslint-disable eqeqeq */
-module.exports = {
-	async formatCSV (obj, ufv, type) {
+const formatCSV = async (obj, ufv, type) => {
 	
-		if (ufv === "campo-grande") {
-			if (type === "production") {
-				return []
-			}
-			if (type === "environmental") {
-				
-				let response = []
-	
+	if (ufv === "campo-grande") {
+		if (type === "production") {
+			return []
+		}
+		if (type === "environmental") {
+			
+			let response = []
+
+			response.push([
+				'Data',
+				'Horario',
+				'Irradiacao (W/m^3)',
+				'PM1 massa (mg/m^3)',
+				'PM2 massa (mg/m^3)',
+				'PM4 massa (mg/m^3)',
+				'PM10 massa (mg/m^3)',
+				'PM1 concentracao (mg/m^3)',
+				'PM2 concentracao (mg/m^3)',
+				'PM4 concentracao (mg/m^3)',
+				'PM10 concentracao (mg/m^3)',
+				'Concentracao padrao (mg/m^3)',
+				'Temperatura (°C)',
+				'Direcao do vento (°)',
+				'Velocidade do vento km/h'
+			])
+
+			for (let i = 0; i < obj.interval.length; i++) {
 				response.push([
-					'Data',
-					'Horario',
-					'Irradiacao (W/m^3)',
-					'PM1 massa (mg/m^3)',
-					'PM2 massa (mg/m^3)',
-					'PM4 massa (mg/m^3)',
-					'PM10 massa (mg/m^3)',
-					'PM1 concentracao (mg/m^3)',
-					'PM2 concentracao (mg/m^3)',
-					'PM4 concentracao (mg/m^3)',
-					'PM10 concentracao (mg/m^3)',
-					'Concentracao padrao (mg/m^3)',
-					'Temperatura (°C)',
-					'Direcao do vento (°)',
-					'Velocidade do vento km/h'
+					obj.date,
+					obj.interval[i],
+					(obj.irradiationInterval[i] == obj.interval[i]) ? obj.irradiation[i] : 0,
+					obj.PM1Particulates[i],
+					obj.PM2Particulates[i],
+					obj.PM4Particulates[i],
+					obj.PM10Particulates[i],
+					obj.PM1Numbers[i],
+					obj.PM2Numbers[i],
+					obj.PM4Numbers[i],
+					obj.PM10Numbers[i],
+					obj.averageSizes[i],
+					obj.temperatures[i],
+					obj.windDirections[i],
+					obj.windSpeeds[i]
 				])
-	
-				for (let i = 0; i < obj.interval.length; i++) {
-					response.push([
-						obj.date,
-						obj.interval[i],
-						(obj.irradiationInterval[i] == obj.interval[i]) ? obj.irradiation[i] : 0,
-						obj.PM1Particulates[i],
-						obj.PM2Particulates[i],
-						obj.PM4Particulates[i],
-						obj.PM10Particulates[i],
-						obj.PM1Numbers[i],
-						obj.PM2Numbers[i],
-						obj.PM4Numbers[i],
-						obj.PM10Numbers[i],
-						obj.averageSizes[i],
-						obj.temperatures[i],
-						obj.windDirections[i],
-						obj.windSpeeds[i]
-					])
-				}
-	
-				return response
-	
 			}
+
+			return response
+
 		}
-	
-		if (ufv === "irece") {
-			if (type === "production") {
-				return []
-			}
-			if (type === "environmental") {
-				return []
-			}
-			if (type === "losses") {
-				return []
-			}
-		}
-	
-		return [];
-	
 	}
+
+	if (ufv === "irece") {
+		if (type === "production") {
+			return []
+		}
+		if (type === "environmental") {
+			return []
+		}
+		if (type === "losses") {
+			return []
+		}
+	}
+
+	return [];
+
 }
+
+export default formatCSV;
