3bdc14a1c706895439e0585047553de58a58a509
Pedro Mihael <pedromvpaes@gmail.com>
2020-02-04 11:15:16 -0400

    Work in progress: select date using calendar

---
+++ src/components/CalendarNav.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+// import { Calendar } from '@progress/kendo-react-dateinputs';
+
+export default class CalendarNav extends Component {
+  render() {
+	return <Calendar />;
+  }
+}
--- src/components/NavBarDates.js
+++ src/components/NavBarDates.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Spinner from '../components/Spinner';
+import CalendarNav from '../components/CalendarNav';
 
 export default class NavBarDates extends Component {
 	constructor(props) {
@@ -17,7 +18,7 @@ export default class NavBarDates extends Component {
 		});
 	}
 
-	UNSAFE_componentWillUpdate (newProps, newState) {
+	UNSAFE_componentWillUpdate(newProps, newState) {
 		if (newProps.date !== this.props.date) {
 			this.setState({ loading: false });
 		}
@@ -26,26 +27,51 @@ export default class NavBarDates extends Component {
 	render() {
 
 		return (
-			<div className="collapse navbar-collapse mx-auto" id="navbar-dates">
-				<ul className="navbar-nav mr-auto">
-					<button className="btn btn-outline-light nav-link " onClick={this.props.handlePrevDateNavigation} onClickCapture={this.handleClick} disabled={this.props.leftNavigationDisabled}>
-						<li className="nav-item flew-grow-1 mx-auto my-auto p-0" id="arrow-left">
-							<i className="material-icons">chevron_left</i>
-						</li>
-					</button>
+			<>
+				<div className="collapse navbar-collapse mx-auto" id="navbar-dates">
+					<ul className="navbar-nav mr-auto">
+						<button className="btn btn-outline-light nav-link " onClick={this.props.handlePrevDateNavigation} onClickCapture={this.handleClick} disabled={this.props.leftNavigationDisabled}>
+							<li className="nav-item flew-grow-1 mx-auto my-auto p-0" id="arrow-left">
+								<i className="material-icons">chevron_left</i>
+							</li>
+						</button>
+						<button type="button" className="btn btn-outline-light nav-link" data-toggle="modal" data-target="#exampleModalCenter">
+							<li className="nav-item flew-grow-1 mx-auto mb-1 pb-1 pt-0" id="now">
+								{this.props.date}
+							</li>
+						</button>
 
-					<li className="nav-item flew-grow-1 mx-auto mt-2" id="now">{this.props.date}</li>
+						<button className="btn btn-outline-light nav-link" onClick={this.props.handleNextDateNavigation} onClickCapture={this.handleClick} disabled={this.props.rightNavigationDisabled}>
+							<li className="nav-item flew-grow-1 mx-auto my-auto p-0" id="arrow-right">
+								<i className="material-icons">chevron_right</i>
+							</li>
+						</button>
 
-					<button className="btn btn-outline-light nav-link" onClick={this.props.handleNextDateNavigation} onClickCapture={this.handleClick} disabled={this.props.rightNavigationDisabled}>
-						<li className="nav-item flew-grow-1 mx-auto my-auto p-0" id="arrow-right">
-							<i className="material-icons">chevron_right</i>
-						</li>
-					</button>
+						<Spinner loading={this.state.loading} />
 
-					<Spinner loading={this.state.loading} />
+					</ul>
+				</div>
 
-				</ul>
-			</div>
+				<div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+					<div className="modal-dialog modal-dialog-centered" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div className="modal-body">
+								{/* <CalendarNav /> */}
+      						</div>
+							<div className="modal-footer">
+								<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+								<button type="button" className="btn btn-primary">Save changes</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
 		);
 	}
 }
